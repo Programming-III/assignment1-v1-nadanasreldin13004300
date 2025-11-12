332b5b0326615e11984d50d1db673f2db3b71454
nadanasreldin13004300 <nada.nasreldin@student.giu-uni.de>
2025-11-12 15:15:09 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,10 @@
-#ifndef MAMMAL_H
+#ifndef MAMMAL_H 
 #define MAMMAL_H
 
-#include "Animal.h"
-
-//define mammal class here
-
-
-
-
-
-
-#endif
+ class mammal : public animal{
+	 private: 
+	  string furColor ;
+	 public: 
+	  mammal(string furColor): animal(string name, int age, bool isHungry);
+}
+#endif 
