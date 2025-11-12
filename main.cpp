72d468f33104c10229da274670819f8b3aa43785
nadanasreldin13004300 <nada.nasreldin@student.giu-uni.de>
2025-11-12 15:34:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,49 @@
 using namespace std;
 
 
-// ============== MAIN FUNCTION ==============
+
+	  class animal::animal(string name, int age, bool isHungry) {
+	  this.name=name;
+	  this.age= age;
+	  this.isHungry= isHungry;
+
+ }
+  void display() {
+	  cout<<name<< endl;
+	  cout<<age<< endl;
+	  cout<<isHungry<<endl;
+	  }
+  void feed() {
+	  if (isHungry == true) {
+		  cout<<""animal is hungry"<< endl;
+	  else 
+	      cout<<"animal is not hungry"<<endl;
+	  }
+	  class mammal :: mammal(string furColor)
+	   : animal(string name, int age, bool isHungry){
+	    this.furColor = furColor;}
+	 class bird :: bird(float wingSpan)
+		 : animal(string name, int age, bool isHungry){ 
+		 this.wingSpan = wingSpan;}
+	 class reptile :: reptile(bool isVenomous)
+		 : animal(string name, int age, bool isHungry){ 
+		 this.isVenomous= isVenomous; 
+		 }
+		
+	 class enclosure::enclosure(int capacity, int currentCount, animal* Animalarr) {
+		 this.capacity=capacity;
+		 this.currentCount=currentCount;
+		 this.Animalarr=Animalarr;
+
+	 }
+   void addAnimal(animal* a){
+  animal* newarr = new animal(Animalarr[] + 1);
+	 void displayAnimals() {
+for(i=0 ; i < Animalarr[] ; i++)
+     cout<<Animalarr[i]<<endl;
+	 }
+
+
 int main() {
     
     
