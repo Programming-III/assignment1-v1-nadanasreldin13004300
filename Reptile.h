6b6044a2365cb6f072b54554fb99c01a83ecef4a
nadanasreldin13004300 <nada.nasreldin@student.giu-uni.de>
2025-11-12 15:17:06 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,10 @@
 #ifndef REPTILE_H
 #define REPTILE_H
-
-#include "Animal.h"
-
-//define reptile class here
-
-
-
-
-
-
-
-
-
+ 
+ class reptile : public animal {
+  private:
+   bool isVenomous;
+   public 
+    reptile(bool isVenomous) : animal(string name, int age, bool isHungry);
+};
 #endif
